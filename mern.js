81944082f03b0385352f8d413a1cbef2e9a22218
Deepakmala <123050483+Deepakmala@users.noreply.github.com>
2023-02-17 18:36:38 +0530

    Add files via upload

---
+++ mern.js
@@ -0,0 +1,7 @@
+// JavaScript for button interactivity
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function() {
+  alert("Thanks for getting started with MERN Stack Development!");
+});
+
